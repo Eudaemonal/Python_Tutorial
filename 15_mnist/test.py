64ea0e9ad5b750e869558e64628631efd50cae98
eudaemonal <eudaemonal@gmail.com>
2018-04-30 22:14:50 +1000

    update train and test

---
+++ 15_mnist/test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import sys
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+# display handwritten digits in test set
+def show_image(mnist):
+    batch = mnist.test.next_batch(1)
+    imvalue = batch[0]
+    plotData = imvalue.reshape(28, 28)
+    plt.gray() # use this line if you don't want to see it in color
+    plt.imshow(plotData)
+    plt.show()
+
+    return imvalue
+
+
+if __name__=="__main__":
+    # load the image dataset
+    mnist = input_data.read_data_sets('data/mnist', one_hot=True)
+
+    imvalue = show_image(mnist)
+
+
+    # Define the model (same as when creating the model file)
+    x = tf.placeholder(tf.float32, [None, 784])
+    W = tf.Variable(tf.zeros([784, 10]))
+    b = tf.Variable(tf.zeros([10]))
+    y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+    init_op = tf.initialize_all_variables()
+    saver = tf.train.Saver()
+    
+    """
+    Load the model.ckpt file
+    file is stored in the same directory as this python script is started
+    Use the model to predict the integer. Integer is returend as list.
+    Based on the documentatoin at
+    https://www.tensorflow.org/versions/master/how_tos/variables/index.html
+    """
+    with tf.Session() as sess:
+        sess.run(init_op)
+        saver.restore(sess, "model.ckpt")
+   
+        prediction=tf.argmax(y,1)
+        print(prediction.eval(feed_dict={x: imvalue}, session=sess))
+
--- 15_mnist/train.py
+++ 15_mnist/train.py
@@ -41,8 +41,6 @@ if __name__=="__main__":
     # each image corresponds to digit 0-9, result in an array of 1x10
     y = tf.placeholder(dtype=tf.float32, shape=[None, 10],name="image_target_onehot")
 
-
-
     # create a simple one layer network
     W = tf.Variable(tf.zeros([784 , 10]))
     b = tf.Variable(tf.zeros([10]))
@@ -53,10 +51,10 @@ if __name__=="__main__":
     y_ = tf.placeholder(tf.float32, [None, 10])
 
     cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+    
     train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
     
-    
-
+    saver = tf.train.Saver()
     # Get started
     sess = tf.InteractiveSession()
     tf.global_variables_initializer().run()
@@ -66,7 +64,12 @@ if __name__=="__main__":
         sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
     correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))    
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))    
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) 
+
+
+    # save the model
+    save_path = saver.save(sess, "model.ckpt")
+    
     print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
     
     # Clean up
